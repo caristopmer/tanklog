ac0fbe0a9d07bea9ba7dcff864a4eedeff524430
caristopmer <chris.mcmenomy@gmail.com>
2017-10-26 14:16:37 -0500

    test sliders starting to work.

--- src/pages/test/test.ts
+++ src/pages/test/test.ts
@@ -14,8 +14,22 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'test.html',
 })
 export class TestPage {
+  private calcium: any;
+  private alkalinity: any;
+  private magnesium: any;
+  private nitrate: any;
+  private phosphate: any;
+  private salinity: any;
+  private date: Date;
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
+    this.calcium = 420;
+    this.alkalinity = 8.5;
+    this.magnesium = 1300;
+    this.nitrate = 0;
+    this.phosphate = 0;
+    this.salinity = 1.026;
+    this.date = new Date();
   }
 
   ionViewDidLoad() {
